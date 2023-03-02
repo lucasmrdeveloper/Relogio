c6435028a193885e6abf8323c2f59208cd40dc79
Lucas Rodrigues <lucasrodrigues.mr@gmail.com>
2023-03-02 14:19:00 -0300

    Estrutura JS

---
+++ script.js
@@ -0,0 +1,48 @@
+(() => {
+    document.addEventListener('DOMContentLoaded', () => {
+        const darkModeBtn = document.querySelector('.dark-mode')
+        darkModeBtn.addEventListener('click', darkMode)
+
+        relogio()
+    })
+
+    const relogio = () => {
+            const containerRelogio = document.querySelector('.box-relogio')
+            const divHora = document.querySelector('.relogio-hora')
+            let divMinutos = document.querySelector('.relogio-minutos')
+            let divSegundos = document.querySelector('.relogio-segundos')
+
+            setInterval(() => {
+                const data = new Date()
+                const hora = data.getHours()
+                const minutos = data.getMinutes()
+                const segundos = data.getSeconds()
+
+                divHora.innerHTML = hora
+                divMinutos.innerHTML = minutos
+                divSegundos.innerHTML = segundos
+
+                if (segundos < 10) {
+                    divSegundos.innerHTML = "0" + segundos
+                }
+
+                if (minutos < 10) {
+                    divMinutos.innerHTML = "0" + minutos
+                }
+            }, 1000)
+    
+            console.log('container relogio', containerRelogio)
+            console.log('div hora', divHora)
+            console.log('div minutos', divMinutos)
+            console.log('div segundos', divSegundos)
+    }
+
+    const darkMode = () => {
+        const body = document.querySelector('body')
+        body.classList.toggle('modo-dark')
+
+
+        console.log('body', body)
+    }
+
+})()
